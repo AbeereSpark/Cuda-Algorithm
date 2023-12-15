0c6f50f6981248394938b2ab4ab2a931792f1554
AbeereSpark <106241042+AbeereSpark@users.noreply.github.com>
2023-12-15 13:09:27 +0500

    Update key.cu

--- CGBN-master/samples/key/key.cu
+++ CGBN-master/samples/key/key.cu
@@ -161,9 +161,9 @@ __global__ void kernel_iterate(cgbn_error_report_t *report, cgbn_mem_t<BITS>* pu
     cgbn_mem_t<BITS> iterationValue;
     iterationValue._limbs[0] = instance;
     cgbn_mem_t<BITS> alteredKey;
-    printf ("Iteration Num : %d\n", instance);
 
     if ((instance < numIterations)) {
+        printf ("Iteration Num : %d\n", instance);
         cgbn_mem_t<BITS> publicKey = publicKeys[0];
         cgbn_mem_t<BITS> operand = operands[0];
 
