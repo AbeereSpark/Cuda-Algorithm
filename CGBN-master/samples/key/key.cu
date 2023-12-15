b932ec7a9c681feb3b03ef8cf364d35c92b064d2
AbeereSpark <106241042+AbeereSpark@users.noreply.github.com>
2023-12-15 16:36:08 +0500

    Update key.cu

--- CGBN-master/samples/key/key.cu
+++ CGBN-master/samples/key/key.cu
@@ -200,20 +200,21 @@ __global__ void kernel_iterate(cgbn_error_report_t *report, cgbn_mem_t<BITS>* pu
         cgbn_load(bn_env, op, &operand);      // load my instance's b value
         cgbn_load(bn_env, iter, &iterationValue);      // load my instance's b value
 
+        cgbn_mul_ui32(bn_env, r, op, instance);
         // Generate a new key by adding (operand * iteration) to the public key
 
-        cgbn_mul(bn_env, rMul, iter, op);
+        // cgbn_mul(bn_env, r, iter, op);
 
-        if (operationType == 'A') 
-        {
-            cgbn_add(bn_env, r, pKey, rMul);
-        } 
-        else if (operationType == 'S') 
-        {
-            cgbn_sub(bn_env, r, pKey, rMul);     
-        }    
+        // if (operationType == 'A') 
+        // {
+        //     cgbn_add(bn_env, r, pKey, rMul);
+        // } 
+        // else if (operationType == 'S') 
+        // {
+        //     cgbn_sub(bn_env, r, pKey, rMul);     
+        // }    
 
-        cgbn_store(bn_env, &alteredKey, rMul);   
+        cgbn_store(bn_env, &alteredKey, r);   
 
         // Now, launch the compare kernel to check for matches
         // Launch the GPU kernel
